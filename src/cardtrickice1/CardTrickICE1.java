4bdd3347fe9a33dbc742d69c7d84c4990830f379
jas1807 <ghatorja@sheridancollege.ca>
2022-05-30 14:39:36 -0400

    version 1 of student project

--- src/cardtrickice1/CardTrickICE1.java
+++ src/cardtrickice1/CardTrickICE1.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package cardtrickice1;
+import cardtrickice1.Card;
+import java.util.Scanner;
 
 /** step1 : generate 7 random cards and store in array - how
  * step 2: take any card input from user suit,number
@@ -18,16 +20,56 @@ public class CardTrickICE1 {
      */
     public static void main(String[] args) 
     {
+        
         Card[] magicHand = new Card[7]; //Array of object
-        for( int i=0;i<magicHand.length;i++)
+        for( int i=0; i<magicHand.length;i++)
         {
             Card c1 = new Card();
-            c1.setValue(2);//use a method to generate random *13
-            c1.setSuits("hearts");//random method suit 
-        }
-        //step 2:take input 
+            c1.setValue((int)(Math.random()*12) + 1);//use a method to generate random *13
+            c1.setSuits(Card.SUITS[(int) (Math.floor(Math.random()*4))]);//random method suit 
+            magicHand[i] = c1;
+            
+            System.out.println(magicHand[i].getValue() + " of " + magicHand[i].getSuits());
+            
+            }
         
-        //step 3: match with array 
-    }
-    
-}
+        //step 2:take input 
+        Scanner input = new Scanner(System.in);
+        
+        
+       System.out.println("please choose the suit: "  + "\n" + "1: diamonds" + "\n" + "2: clubs" + "\n" + "3: Spades"+"\n" + "4: hearts" );
+        int userSuit = input.nextInt();
+        
+        System.out.println("please choose the value (1 to 12)");
+       int userValue = input.nextInt();
+       
+        String response = "card not found";
+        
+        for(int i=0; i < 7; i++){
+        
+        //step 3: match with array
+       
+      if(magicHand[i].getSuits().equals( magicHand[userSuit -1].getSuits()) && userValue == magicHand[i].getValue()) {
+      
+      response = "card found";
+    System.out.println(response);
+    System.exit(0);
+      }
+        }
+     
+      System.out.println(response);
+      
+      
+      
+  
+        
+        
+        
+        
+        
+        
+        
+          }
+       
+        }
+        
